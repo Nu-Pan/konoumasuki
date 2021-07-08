807e33910cc609e781bed584836d85c4b8a63d78
NU-Pan <nupan909@gmail.com>
2021-07-09 05:13:31 +0900

    ある程度コメント入れた

--- parameter_solver/parameter_solver.py
+++ parameter_solver/parameter_solver.py
@@ -3,17 +3,29 @@ from sympy import *
 from typing import Dict, List, Any
 from sympy.plotting import plot
 
+'''
+# sympy ヘルパ
+'''
+
 def makesym() -> Symbol:
     '''
-    ダミーの適当なシンボルを生成する
+    シンボルを生成する。
+    シンボル名は後で更新するので適当でいい。
     '''
     return Symbol('undef')
 
 def Lerp( min_value: Symbol, max_value: Symbol, ratio: Symbol ) -> Symbol:
+    '''
+    区間 [min_value, max_value] 中の比率 ratio の値を返す。
+    ratio は区間 [0.0, 1.0] に従う。
+    '''
     length = max_value - min_value
     return length * ratio + min_value
 
 def clamp( min_value: Symbol, max_value: Symbol, value: Symbol ) -> Symbol:
+    '''
+    value を区間 [min_value, max_value] にクランプする。
+    '''
     return Piecewise( 
         (min_value, value < min_value),
         (max_value, value > max_value),
@@ -21,9 +33,10 @@ def clamp( min_value: Symbol, max_value: Symbol, value: Symbol ) -> Symbol:
     )
 
 '''
-リーフパラメータ
+# リーフパラメータ定義
 入力として与えるべきパラメータ。
 '''
+
 # ウマ娘基礎ステータス
 スピード = makesym()
 スタミナ = makesym()
@@ -65,7 +78,11 @@ def clamp( min_value: Symbol, max_value: Symbol, value: Symbol ) -> Symbol:
 # 他
 育成モード補正 = makesym()
 
-# ルートパラメータ
+'''
+# ルートパラメータ定義
+計算式の左辺にしか存在しないパラメータ。
+'''
+
 基礎目標速度 = makesym()
 通常目標速度 = makesym()
 ポジションキープ目標速度 = makesym()
@@ -79,7 +96,11 @@ def clamp( min_value: Symbol, max_value: Symbol, value: Symbol ) -> Symbol:
 レーン変更加速度 = makesym()
 レーン変更実際速度 = makesym()
 
-# python 上と sympy 上で変数名を一致させる
+'''
+# sympy シンボル名更新
+python 上と sympy 上で変数名を一致させる。
+'''
+
 base_locals = locals()
 locals().update([
     (k, Symbol(k))
@@ -87,12 +108,19 @@ locals().update([
     if type(base_locals[k]) is Symbol
 ])
 
-# ノードパラメータ
+'''
+# ノードパラメータ定義
+リーフパラメータとノードパラメータをつなぐ中間パラメータ。
+'''
+
+# ウマ娘基礎パラメータ
 補正スピード = スピード * やる気補正 * 基本速度補正 + バ場状態パラ補正 + 育成モード補正 + スキル補正スピード
 補正賢さ = 賢さ * やる気補正 * 脚質補正 + 育成モード補正 + スキル補正賢さ
 補正スタミナ = スタミナ * やる気補正 + バ場状態パラ補正 + 育成モード補正 + スキル補正スタミナ
 補正パワー = パワー * やる気補正 + バ場状態パラ補正 + 育成モード補正 + スキル補正パワー
 補正根性 = 根性 * やる気補正 + バ場状態パラ補正 + 育成モード補正 + スキル補正根性
+
+# 速度関係
 レース基準速度 = 20 - ( 2000 - レース距離 ) * 0.001
 賢さランダム補正上限 = ( 補正賢さ / 5500 ) * log( 補正賢さ * 0.1 )
 賢さランダム補正下限 = 賢さランダム補正上限 - 0.65
@@ -100,13 +128,21 @@ locals().update([
 上り坂補正 = - abs( 100 * tan( 傾斜角 * 0.017453 ) ) * 200 / 補正パワー
 下り坂補正 = 0.3 + abs( 100 * tan( 傾斜角 * 0.017453 ) ) / 10.0
 坂補正 = Piecewise( (下り坂補正, 傾斜角 < 0), (上り坂補正, 傾斜角 >= 0) )
-体力消耗速度補正 = ( 現在速度 - レース基準速度 + 12 ) ** 2 / 144
-スパート補正 = 1 + 200 / sqrt( 600 * 補正根性 )
-スタート補正 = 1.0 + 0.05 * ( 現在レーン距離 / 最大レーン距離 )
-順位補正 = 1.0 + 0.001 * 順位
-内側移動補正 = - ( 1.0 + 現在レーン距離 )
 
-# ルートパラメータ
+# 体力関係
+体力消耗速度補正 = ( 現在速度 - レース基準速度 + 12 ) ** 2 / 144
+体力消耗速度スパート補正 = 1 + 200 / sqrt( 600 * 補正根性 )
+
+# レーン変更関係
+レーン変更スタート補正 = 1.0 + 0.05 * ( 現在レーン距離 / 最大レーン距離 )
+レーン変更順位補正 = 1.0 + 0.001 * 順位
+レーン変更内側移動補正 = - ( 1.0 + 現在レーン距離 )
+
+'''
+# ルートパラメータ式定義
+ルートパラメータの計算式。
+solve の対象なので Eq にしている。
+'''
 equations = [
 Eq(
     基礎目標速度,
@@ -142,11 +178,11 @@ Eq(
 ),
 Eq(
     体力消耗速度,
-    20 * 体力消耗速度補正 * バ場状態体力消費速度補正 * ウマ状態補正 * スパート補正
+    20 * 体力消耗速度補正 * バ場状態体力消費速度補正 * ウマ状態補正 * 体力消耗速度スパート補正
 ),
 Eq(
     レーン変更目標速度,
-    0.02 * ( 0.3 + 0.001 * 補正パワー ) * スタート補正 * 順位補正
+    0.02 * ( 0.3 + 0.001 * 補正パワー ) * レーン変更スタート補正 * レーン変更順位補正
 ),
 Eq(
     レーン変更加速度,
@@ -154,9 +190,9 @@ Eq(
 ),
 Eq(
     レーン変更実際速度,
-    clamp( 0, 0.6, 現在速度 + スキル補正速度 ) * 内側移動補正
+    clamp( 0, 0.6, 現在速度 + スキル補正速度 ) * レーン変更内側移動補正
 )
 ]
 
-# 補正後スピードを満たすのに必要なスピードを解く
-solve(equations, スピード)
+# ↓みたいな感じで解く
+# solve(equations, スピード)
